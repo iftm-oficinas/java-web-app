b2a88bd9064ec0f6711006def40c7aad08455014
Marlon Freitas <marlonmacf@gmail.com>
2016-08-11 10:31:31 -0300

    Spring Boot and AngularJS Integration Tutorial

--- src/main/java/domain/controllers/MainController.java
+++ src/main/java/domain/controllers/MainController.java
@@ -2,27 +2,37 @@ package domain.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import domain.services.UserService;
-import domain.entities.User;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.ui.Model;
 
-import java.util.*;
+//import domain.services.UserService;
+//import domain.entities.User;
+
+//import java.util.*;
 
 @Controller
 public class MainController {
 
+    /*
     @Autowired
     private UserService userService;
     
-    @RequestMapping("/")
+    @RequestMapping("/", method = RequestMethod.GET)
     String index(Model model) {
         HashMap response = new HashMap();
+        
         userService.insert("marlonmacf@gmail.com", "Marlon Andrel", "*****");
         response.put("users", userService.fetchAll());
         model.addAttribute("users", response.get("users"));
         
         return "main/index";
+    }
+    */
+    
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String homepage(){
+        return "index";
     }
 }
