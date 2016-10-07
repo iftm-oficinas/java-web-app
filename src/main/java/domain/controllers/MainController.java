87f1bf560c04bdff5ab92161c6ce8ff24960139e
Marlon Freitas <marlon_freitas@qat.com>
2016-10-07 16:47:56 -0300

    Updated the project to an stable version.

--- src/main/java/domain/controllers/MainController.java
+++ src/main/java/domain/controllers/MainController.java
@@ -4,35 +4,48 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 
-//import domain.services.UserService;
-//import domain.entities.User;
+import domain.services.UserService;
+import domain.entities.User;
 
-//import java.util.*;
+import java.util.*;
 
 @Controller
 public class MainController {
 
-    /*
     @Autowired
     private UserService userService;
     
-    @RequestMapping("/", method = RequestMethod.GET)
-    String index(Model model) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    String index() {   
+        return "main/index";
+    }
+    
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    String fetch(Model model) {
         HashMap response = new HashMap();
         
-        userService.insert("marlonmacf@gmail.com", "Marlon Andrel", "*****");
         response.put("users", userService.fetchAll());
         model.addAttribute("users", response.get("users"));
         
         return "main/index";
     }
-    */
     
-    @RequestMapping(value="/", method = RequestMethod.GET)
-    public String homepage(){
-        return "index";
+    @RequestMapping(value = "/user/store", method = RequestMethod.GET)
+    String store(Model model) {
+        HashMap response = new HashMap();
+        
+        try {
+            userService.insert("marlonmacf@gmail.com", "Marlon Andrel", "*****");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        response.put("users", userService.fetchAll());
+        model.addAttribute("users", response.get("users"));
+        
+        return "main/index";
     }
 }
