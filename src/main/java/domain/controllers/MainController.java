aab08182f58b56a6cf0fe65773354a5beaa4c639
Marlon Freitas <marlon_freitas@qat.com>
2016-10-10 11:23:19 -0300

    commit

--- src/main/java/domain/controllers/MainController.java
+++ src/main/java/domain/controllers/MainController.java
@@ -1,51 +1,50 @@
 package domain.controllers;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-
 import domain.services.UserService;
-import domain.entities.User;
-
-import java.util.*;
 
 @Controller
 public class MainController {
 
-    @Autowired
-    private UserService userService;
-    
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    String index() {   
-        return "main/index";
-    }
-    
-    @RequestMapping(value = "/user", method = RequestMethod.GET)
-    String fetch(Model model) {
-        HashMap response = new HashMap();
-        
-        response.put("users", userService.fetchAll());
-        model.addAttribute("users", response.get("users"));
-        
-        return "main/index";
-    }
-    
-    @RequestMapping(value = "/user/store", method = RequestMethod.GET)
-    String store(Model model) {
-        HashMap response = new HashMap();
-        
-        try {
-            userService.insert("marlonmacf@gmail.com", "Marlon Andrel", "*****");
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        
-        response.put("users", userService.fetchAll());
-        model.addAttribute("users", response.get("users"));
-        
-        return "main/index";
-    }
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	String index() {
+		return "main/index";
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	String fetch(Model model) {
+		HashMap<String, Iterable<?>> response = new HashMap<String, Iterable<?>>();
+
+		response.put("users", userService.fetchAll());
+		model.addAttribute("users", response.get("users"));
+
+		return "main/index";
+	}
+
+	@RequestMapping(value = "/user/store", method = RequestMethod.GET)
+	String store(Model model) {
+		HashMap<String, Iterable<?>> response = new HashMap<String, Iterable<?>>();
+
+		try {
+			userService.insert("marlonmacf@gmail.com", "Marlon Andrel", "*****");
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+
+		response.put("users", userService.fetchAll());
+		model.addAttribute("users", response.get("users"));
+
+		return "main/index";
+	}
 }
