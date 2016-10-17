c93fa6df47d34f0a3fdb3b49066211a5017efb4b
Marlon Andrel <marlonmacf@gmail.com>
2016-10-17 16:52:01 -0200

    Updated the core inheritance in the javascript objects.

--- src/main/resources/static/apps/Core.js
+++
@@ -1,6 +0,0 @@
-// Implementing the Parasitic Combination Inheritance Pattern.
-function inheritPrototype(childObject, parentObject) {
-	var copyOfParent = Object.create(parentObject.prototype);
-	copyOfParent.constructor = childObject;
-	childObject.prototype = copyOfParent;
-};
--- src/main/resources/static/apps/main/js/ChildClass.js
+++ src/main/resources/static/apps/main/js/ChildClass.js
@@ -2,8 +2,13 @@ function ChildClass(value) {
 	ParentClass.call(this, value);
 };
 
-inheritPrototype(ChildClass, ParentClass);
+ChildClass.prototype = Object.create(ParentClass.prototype);
+ChildClass.prototype.constructor = ChildClass;
 
 ChildClass.prototype.method = function() {
 	console.log("Implemented!");
+};
+
+ChildClass.prototype.newMethod = function() {
+	console.log("Implemented a new method too!");
 };
