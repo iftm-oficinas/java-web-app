73581a5a00e3acd70fbea9284f4e51640f70bab4
Marlon Andrel <marlonmacf@gmail.com>
2016-11-04 11:51:16 -0200

    Updated the application to handle multples active profiles

---
+++ src/main/java/domain/Development.java
@@ -0,0 +1,13 @@
+package domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Development {
+
+    public static void main(String[] args) {
+    	System.setProperty("spring.profiles.active", "development");
+        SpringApplication.run(Application.class, args);
+    }
+}
